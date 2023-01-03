eb6411be840196dc17a295e5e4f6cdb15529f538
unknown <sharmadharmesh1998@gmail.com>
2023-01-03 22:46:19 +0530

    chatbar added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import "./MainStyles.css";
+import profile from "../images/moonlogocompressed.png";
 
 function Main(){
     return(
         <>
             <section className='main-section'>
                 <div className="main-container">
+                    <div className="message-bar-container">
+
+                        <div className="profile-container">
+                            <img src={profile} alt="profile" className='profile-img'/>
+                        </div>
+
+                        <div className="message-bar">
+                            This is message bar 
+                        </div>
+                    </div>
+
+
+
+
+
+{/* <<<---------------------    Search bar and btn    --------------------->>>  */}
 
                     <div className="search-and-btn-container">
-                    <textarea cols="5" rows="1" className='main-input' placeholder='Enter your text...'>
-                    
-                    </textarea>
-                    <button className='main-search-btn'>
-                        Go
-                        <i class="fa-solid fa-magnifying-glass search-icon"></i>
-                    </button>
+                        <textarea cols="5" rows="1" className='main-input' placeholder='Enter your text...'>
+                        
+                        </textarea>
+                        <button className='main-search-btn'>
+                            Go
+                            <i class="fa-solid fa-magnifying-glass search-icon"></i>
+                        </button>
                     </div>
                 </div>
             </section>
